e4d178576abe08b4391ff4fc82cd74eef25539f5
pong <jp19reyes@gmail.com>
2020-02-02 20:44:19 +0800

    main menu form

---
+++ login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Despro
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            mainMenu mainMenu = new mainMenu();
+            Hide();
+            mainMenu.Show();
+        }
+    }
+}
